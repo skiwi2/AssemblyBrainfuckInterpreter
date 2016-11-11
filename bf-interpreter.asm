d5ca5b85cab0df033857346a6452a6789ecc220d
skiwi <frank_skiwi@hotmail.com>
2016-11-11 21:19:49 +0100

    The program now crashes when attempting to read a too big program, also renamed the read_program_* labels to store_program_*.

--- bf-interpreter.asm
+++ bf-interpreter.asm
@@ -9,6 +9,8 @@ segment _DATA public align=4 class=DATA use32
 msg_memoryamount    db      "Enter how much memory (in bytes) does your Brainfuck program needs: ", 0
 msg_bfprogram       db      "Enter your Brainfuck program (use Enter exclusively to continue): ", 0
 
+error_programsize   db      "Fatal: The given Brainfuck program exceeded the given memory size.", 0
+
 segment _BSS public align=4 class=BSS use32
 
 max_bf_program_size     resd 1
@@ -20,10 +22,10 @@ group DGROUP _BSS _DATA
 segment _TEXT public align=1 class=CODE use32
         global  _asm_main
 _asm_main:
-    enter   0,0             ; setup routine
+    enter   0,0                         ; setup routine
     pusha
 ;
-; read Brainfuck program from console input
+; store Brainfuck program from console input
 ;    
     mov     eax, msg_memoryamount
     call    print_string
@@ -44,20 +46,35 @@ _asm_main:
     mov     ecx, [max_bf_program_size]
     xor     edx, edx
     
-read_program_loop:
+store_program_loop:
     call    read_char
     
     cmp     eax, NEWLINE_CODE           ; stop reading on newline
-    jz      short read_program_done
+    jz      short store_program_done
+    
+    cmp     edx, ecx                    ; error if exceeded program size
+    jz      short error_exit_programsize
     
     mov     [bf_program + edx], eax
     
     inc     edx
-    loop    read_program_loop
+    jmp     short store_program_loop
     
-read_program_done:
+store_program_done:
     mov     [bf_program_size], edx
     
+    jmp     short normal_exit
+    
+; TODO: add reasonable way to support multiple error messages
+error_exit_programsize:
+    mov     eax, error_programsize
+    call    print_string                ; TODO: this should really print to stderr
+    popa
+    mov     eax, -1
+    leave
+    ret
+    
+normal_exit:
     popa
     mov     eax, 0
     leave
